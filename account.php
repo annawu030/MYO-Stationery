259c901e2077dac50b9b960a29430873c6ee4dd3
Sarah Meng <shm3px@virginia.edu>
2019-04-30 20:58:54 -0400

    add link to change username, might not work yet

--- account.php
+++ account.php
@@ -25,9 +25,25 @@
             // if user is logged in, display saved stuff from database
             if (isset($_SESSION["username"])) {
                 // greet
-                echo "<p>Welcome, " . htmlspecialchars($_SESSION["username"]) . "! Here are your creations.</p>";
+                echo "<p>Welcome, " . htmlspecialchars($_SESSION["username"]) . "!</p>";
                 
-                // show the pretties from database
+                // give option to change username
+
+                // currently need to link to Angular (on a different port)
+                // solution for linking found here:
+                // https://stackoverflow.com/questions/6016120/relative-url-to-a-different-port-number-in-a-hyperlink/13522508#13522508
+?>
+<script type="text/javascript">
+document.write('<p>You can change your username <a href="' + window.location.protocol + '//' + window.location.hostname + ':4200' + '" >here</a>.');
+</script>
+<?php
+                // the regular implementation:
+                //echo "<p>You can change your username <a href="changeusername.html">here</a>.</p>";
+
+                // now...
+                echo "<p>Here are your creations.</p>";
+                
+                // time to show the pretties from database
                 
             }
             // else, if user is a guest, display saved stuff from cookies?
